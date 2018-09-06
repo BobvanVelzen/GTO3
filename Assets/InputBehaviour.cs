ca28cf58b5ca24359aa4bc3de61f2be7375fba46
nodenba <nodenba@hotmail.com>
2018-09-06 14:44:28 +0200

    First Commit
    Hope this works

---
+++ Assets/InputBehaviour.cs
@@ -0,0 +1,149 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputBehaviour : MonoBehaviour {
+
+    private bool startedDoubleTap;
+    private float timeSinceFirstTap;
+
+    private Transform card;
+    private float dist;
+    private Vector3 dragOffset;
+    private Plane plane;
+
+    public float TapInterval = 0.5f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (startedDoubleTap)
+        {
+            timeSinceFirstTap += Time.deltaTime;
+            if (timeSinceFirstTap > TapInterval)
+            {
+                startedDoubleTap = false;
+                timeSinceFirstTap = 0;
+            }
+        }
+
+        if (Input.touchCount == 0) return;
+        Touch touch = Input.GetTouch(0);
+
+        if (touch.phase == TouchPhase.Began)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(touch.position);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform.tag == "Card")
+                {
+                    card = hit.transform;
+                    plane.SetNormalAndPosition(Camera.main.transform.forward, card.position);
+                    Ray cardRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+                    float dist;
+                    plane.Raycast(ray, out dist);
+                    dragOffset = card.position - ray.GetPoint(dist);
+                    Debug.Log("Got card!");
+                }
+            }
+        }
+
+        if (touch.phase == TouchPhase.Moved)
+        {
+            if (card != null)
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                float dist;
+                plane.Raycast(ray, out dist);
+                Vector3 v3Pos = ray.GetPoint(dist);
+                card.position = v3Pos + dragOffset;
+            }
+        }
+
+        if (touch.phase == TouchPhase.Ended)
+        {
+            startedDoubleTap = true;
+            card = null;
+        }
+    }
+
+    public Vector3 GetTarget()
+    {
+        if (Input.touchCount == 0) return Vector3.zero;
+        Touch touch = Input.GetTouch(0);
+
+        if (touch.phase == TouchPhase.Ended)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(touch.position);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                return hit.point;
+            }
+        }
+        return Vector3.zero;
+    }
+
+    public bool Tap()
+    {
+        if (Input.touchCount == 0) return false;
+        Touch touch = Input.GetTouch(0);
+
+        if (touch.phase == TouchPhase.Ended)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public bool DoubleTap()
+    {
+        if (Input.touchCount == 0) return false;
+        Touch touch = Input.GetTouch(0);
+
+        if (touch.phase == TouchPhase.Ended)
+        {
+            if (startedDoubleTap)
+            {
+                if (timeSinceFirstTap < TapInterval)
+                {
+                    startedDoubleTap = false;
+                    timeSinceFirstTap = 0;
+                    return true;
+                }
+            }
+            else
+            {
+                startedDoubleTap = true;
+                return false;
+            }
+        }
+
+        return false;
+    }
+
+    public bool Drag()
+    {
+        if (Input.touchCount == 0) return false;
+        else return true;
+    }
+
+    public Vector3 GetMovementDirection()
+    {
+        if (Input.touchCount == 0) return Vector2.zero;
+        Touch touch = Input.GetTouch(0);
+
+        Vector2 screenPosition = touch.position;
+        Vector2 middleOfScreen = new Vector2(Screen.width / 2f, Screen.height / 2f);
+
+        Vector2 positionDelta = screenPosition - middleOfScreen;
+
+        return positionDelta.normalized;
+    }
+}
---
+++ Assets/Movement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(InputBehaviour))]
+public class Movement : MonoBehaviour {
+
+    private InputBehaviour _input;
+
+    public Vector3 target;
+    public float smoothTime = 0.05f;
+
+    public float movementSpeed = 5f;
+
+	// Use this for initialization
+	void Start () {
+        _input = GetComponent<InputBehaviour>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (_input.Drag())
+        {
+            Vector3 movementDirection = _input.GetMovementDirection();
+            Vector3 movement = new Vector3(movementDirection.x, 0, movementDirection.y);
+            transform.Translate(movement * Time.deltaTime * movementSpeed);
+        }
+
+        if (_input.Tap())
+        {
+            //Vector3 iTarget = _input.GetTarget();
+            //if (iTarget != Vector3.zero) target = iTarget;
+
+            Debug.Log("Tap");
+        }
+
+        if (_input.DoubleTap())
+        {
+            Debug.Log("DoubleTap");
+            Renderer r = GetComponentInChildren<Renderer>();
+            r.material.SetColor("_Color", Random.ColorHSV());
+        }
+
+        //if (target != null)
+        //{
+        //    Vector3 targetPosition = new Vector3(target.x, transform.position.y, target.z);
+        //    transform.position = Vector3.Lerp(transform.position, targetPosition, smoothTime);
+        //}
+    }
+}
